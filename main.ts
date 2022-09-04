ce341f7989f8d2f19667d2ac766fae3a0fa7d273
ouret1060 <70791389+ouret1060@users.noreply.github.com>
2022-09-04 17:17:51 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
-basic.showIcon(IconNames.Heart)
+let angle = 0
+pins.servoWritePin(AnalogPin.P0, 90)
+basic.pause(5000)
+let servo = 90
+
 basic.forever(function () {
-	
+    angle = input.compassHeading()
+    angle = (angle > 180) ? angle - 360 : angle
+    if (servo < 180 && angle > 3) {
+        servo += 2
+    } else if (servo > -180 && angle < -3) {
+        servo -= 2
+    }
+    pins.servoWritePin(AnalogPin.P0, servo)
+    serial.writeString("angle = " + angle + ", servo = " + servo + "\r\n")
+    basic.pause(100)
 })
